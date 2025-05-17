40304c5a0f443c4ab2946424e6d8b35bc30d0ca5
danylo8 <ozai.avatar@gmail.com>
2025-05-16 21:22:55 -0400

    Updated Main.java
    Leandro did this (for whatever reason VSCode didn't want to commit it)

--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
+import java.util.ArrayList();
+
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+       String[] cardNum = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+       String[] cardType = {"Heart", "Spades", "Clubs", "Diamonds", "Joker"};
+       String[] playerDeck = new ArrayList[]
+       String[] oppsDeck =
+   }
+
+
+   public void shuffleDeck(deck[])
+   {
+
+
+   }
+
+
+   public void intitalizeDeck(deck[])
+   {
+      
+   }
+}
+
 
