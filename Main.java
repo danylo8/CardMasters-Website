4a2a31fbea0f4bedd2cb6c779073ce8f479c72b7
LeandroC192 <Leandroc16@nycstudents.net>
2025-05-18 01:56:21 +0000

    Added code for shuffleDeck + initializeDeck + drawFromDeck

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList();
+import java.util.ArrayList;
 
 public class Main
 {
@@ -6,17 +6,40 @@ public class Main
     {
         String[] cardNum = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] cardType = {"Heart", "Spades", "Clubs", "Diamonds", "Joker"};
-        String[] playerDeck = new ArrayList[]
-        String[] oppsDeck =
+        ArrayList<String> playerDeck = new ArrayList<>();
+        ArrayList<String> oppsDeck =  new ArrayList<>();
+        ArrayList<String> mainDeck = new ArrayList<>();
     }
 
-    public void shuffleDeck(deck[])
+    public void shuffleDeck()
     {
-
+        String temp = "";
+        for(int i = 0; i < mainDeck.length; i++)
+        {
+            temp = mainDeck[i];
+            mainDeck[i] = mainDeck[Math.random()*mainDeck.length];
+            mainDeck[Math.random()*mainDeck.length] = temp;
+        }
     }
 
-    public void intitalizeDeck(deck[])
+    public void intitalizeDeck()
     {
-        
+        for(int i = 0; i < cardType.length; i++)
+        {
+            for(int j = 0;j < cardNum.length; j++)
+            {
+                mainDeck.add(cardNum[j] + " of " + cardType[i]);
+            }
+        }
+    }
+
+    public String drawFromDeck()
+    {
+        if(mainDeck.length = 0)
+        {
+            return null;
+        }else{
+            return deck.remove[0];
+        }
     }
 }
