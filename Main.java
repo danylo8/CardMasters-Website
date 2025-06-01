d5ba15eeeb22fb002f8d8218b855179b039acd43
LeandroC192 <Leandroc16@nycstudents.net>
2025-05-31 20:00:08 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -11,16 +13,27 @@ public class Main
     static Scanner scan = new Scanner(System.in);
     int playerCounts = 0;
     int oppsCounts = 0;
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         System.out.println("What card game would you like to play?");
+        System.out.println("There is Cuarenta, Spit, Spoons, and BullSh!t");
+        System.out.println("Type in the exact title of the game you would like to play");
         String choice = scan.nextLine().trim();
-        if(choice.equals("cuarenta"))
+        if(choice.equals("Cuarenta"))
         {
+            System.out.println("Sorry, i don't know how to speak Spanish, or Portuguese");
             Cuarenta gameCuarenta = new Cuarenta();
-        }else if(choice.equals("spit"))
+        }else if(choice.equals("Spit"))
         {
             Spit spitGame = new Spit();
+        }else if(choice.equals("Spoons"))
+        {
+            System.out.println("Sorry, but someone took the last of the disposible spoons to some party last night");
+            //Spoons spoonGame = new Spoons();
+        }else if(choice.equals("BullSh!t"))
+        {
+            System.out.println("Hey! No curse words!");
+            //Bullsht bsGame = new Bullsht();
         }
     }
 
@@ -37,7 +50,7 @@ public class Main
     }
 
     //Try to figure out a way to fix the code
-   public static void shuffleDeck(ArrayList<String> deck)
+    public static void shuffleDeck(ArrayList<String> deck)
     {
         //String temp = "";
         //for(int i = 0; i < deck.length; i++)
