c1fa83ee825fe05200e242fa7704d1f4038353a5
LeandroC192 <Leandroc16@nycstudents.net>
2025-06-04 11:10:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,60 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main
 {
-
+    static String[] cardNum = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    static String[] cardType = {"Hearts", "Spades", "Clubs", "Diamonds"};
+    static ArrayList<String> mainDeck = new ArrayList<>();
+    static ArrayList<String> activeDeck = new ArrayList<>();
     static Scanner scan = new Scanner(System.in);
+    int playerCounts = 0;
+    int oppsCounts = 0;
     public static void main(String[] args)
     {
         System.out.println("What card game would you like to play?");
+        System.out.println("There is Cuarenta, Spit, Spoons, and BullSh!t");
+        System.out.println("Type in the exact title of the game you would like to play");
         String choice = scan.nextLine().trim();
         if(choice.equals("Cuarenta"))
         {
-            Cuarenta game = new Cuarenta();
-        }
-
-        if(choice.equals("BS"))
+            //System.out.println("Sorry, i don't know how to speak Spanish, or Portuguese");
+            Cuarenta gameCuarenta = new Cuarenta();
+        }else if(choice.equals("Spit"))
         {
-            BS game= new BS();
+            Spit spitGame = new Spit();
+        }else if(choice.equals("Spoons"))
+        {
+            //System.out.println("Sorry, but someone took the last of the disposible spoons to some party last night");
+            Spoons spoonGame = new Spoons();
+        }else if(choice.equals("BullSh!t"))
+        {
+            //System.out.println("Hey! No curse words!");
+            Bullsht bsGame = new Bullsht();
         }
     }
 
+    public static void intitalizeDeck(ArrayList<String> deck)
+    {
+        deck.clear();
+        for(int i = 0; i < cardType.length; i++)
+        {
+            for(int j = 0;j < cardNum.length; j++)
+            {
+                deck.add(cardNum[j] + " of " + cardType[i]);
+            }
+        }
+    }
 
+    //Try to figure out a way to fix the code
+    public static void shuffleDeck(ArrayList<String> deck)
+    {
+        //String temp = "";
+        //for(int i = 0; i < deck.length; i++)
+        {
+            //temp = deck[i];
+            //deck[i] = deck[Math.random()*deck.length];
+            //deck[Math.random()*deck.length] = temp;
+            Collections.shuffle(deck);
+        }
+    }
 }
