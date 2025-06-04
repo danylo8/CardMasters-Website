1ba01d04cc51e4cc229c378959def648f2b45dff
danylo8 <ozai.avatar@gmail.com>
2025-06-04 11:13:40 -0400

    finished spoons?

--- Spoons.java
+++ Spoons.java
@@ -15,9 +15,14 @@ public class Spoons {
     static ArrayList<String> playerDeck = new ArrayList<>();
     static ArrayList<String> oppsDeck =  new ArrayList<>();
     static ArrayList<String> discardDeck = new ArrayList<>();
-    static String playerCardTarget;
-    static String oppCardTarget;
+    static String oppCardTarget=null;
+    static String cardToDiscard=null;
     static boolean isSpoonTaken=false;
+    static int randomCardIndex=0;
+    static int discardCardIndex=0;
+    static boolean playerTurn=true;
+    static boolean oppsTurn=false;
+    static int cardsThatMatch=0;
 
 
     public Spoons() {
@@ -33,6 +38,17 @@ public class Spoons {
                 playerDeck.add(originalDeck.get(i));
             } else {
                 oppsDeck.add(originalDeck.get(i));
+            }
+        }
+
+
+        while(isSpoonTaken==false) {
+            if (playerTurn == true) {
+                playerMove();
+            }
+
+            if (oppsTurn == true) {
+                oppMove();
             }
         }
     }
@@ -60,43 +76,93 @@ public class Spoons {
 
         System.out.println("Your turn. Your cards: " + playerDeck);
 
+        String firstCardinPlayerDeck = playerDeck.get(0);
+        String firstValue = firstCardinPlayerDeck.substring(0, firstCardinPlayerDeck.indexOf(" "));
+        cardsThatMatch = 1;
+
         for (int i=1; i<playerDeck.size(); i++)
         {
-            System.out.println(i + ": " + playerDeck.get(i));
-        }
-            System.out.println("Type in the number of the card you want to discard");
-            String choice1 = scan.nextLine().trim();
-            String playedCard = playerDeck.remove(Integer.parseInt(choice1));
-            oppsDeck.add(playedCard);
-            System.out.println("You discard: " + playedCard);
-            expectedCard = expectedCard + 1;
-            for (int i=0; i<1; i++)
+            String nextCardinPlayerDeck = playerDeck.get(i);
+            String nextValue = nextCardinPlayerDeck.substring(0, nextCardinPlayerDeck.indexOf(" "));
+
+            if (nextValue.equals(firstValue))
             {
-                playerDeck.add(originalDeck[i])
+                cardsThatMatch+=1;
             }
+        }
+
+        if (cardsThatMatch==4) {
+            System.out.println("You picked up a spoon. You win.");
+            isSpoonTaken=true;
+            playerTurn = false;
+            oppsTurn = false;
+        }
+        else {
+            for (int i=0; i<playerDeck.size(); i++) {
+                System.out.println(i + ": " + playerDeck.get(i));
+            }
+            System.out.println("Type in the number of the card you want to pass to opp: ");
+            String choice = scan.nextLine().trim();
+            String playedCard = playerDeck.remove(Integer.parseInt(choice));
+            oppsDeck.add(playedCard);
+            System.out.println("You passed: " + playedCard);
+            playerDeck.add(originalDeck.remove(0));
 
             playerTurn = false;
             oppsTurn = true;
         }
+        }
 
-    public void oppMove()
-    {
+    public void oppMove() {
 
         System.out.println("Opp's turn.");
 
-        for (int i=0; i<oppsDeck; i++)
-        {
-            if (Math.random()<0.3) {
-                discardOppCardIndex = i;
+        String firstCardinOppDeck = oppsDeck.get(0);
+        String firstValue = firstCardinOppDeck.substring(0, firstCardinOppDeck.indexOf(" "));
+        cardsThatMatch = 1;
+        for (int i=1; i<oppsDeck.size(); i++) {
+            String nextCardinOppDeck = oppsDeck.get(i);
+            String nextValue = nextCardinOppDeck.substring(0, nextCardinOppDeck.indexOf(" "));
+
+            if (nextValue.equals(firstValue))
+            {
+                cardsThatMatch+=1;
             }
 
-        }
-        discardDeck.add(playedCard);
-        System.out.println("Opp discard: " + playedCard);
-        expectedCard = expectedCard + 1;
-        playerTurn = false;
-        oppsTurn = true;
-    }
+            if (!nextValue.equals(firstValue))
+            {
+                cardToDiscard=nextCardinOppDeck;
+                discardCardIndex=i;
+            }
 
-}
+
+        }
+
+        if (cardsThatMatch>=2) {
+
+            oppCardTarget=firstValue;
+        }
+
+        if (cardsThatMatch==4) {
+
+            System.out.println("Opp picked up a spoon. Opp wins.");
+            isSpoonTaken=true;
+            playerTurn = false;
+            oppsTurn = false;
+        }
+
+        if (cardToDiscard==null) {
+
+            randomCardIndex=(int)(Math.random()*oppsDeck.size());
+            cardToDiscard=oppsDeck.remove(randomCardIndex);
+        }
+        else {
+            oppsDeck.remove(discardCardIndex);
+        }
+        discardDeck.add(cardToDiscard);
+        System.out.println("Opp discard: " + cardToDiscard);
+        oppsTurn = false;
+        playerTurn = true;
+    }
+    }
 
