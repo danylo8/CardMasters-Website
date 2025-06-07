3b9b4c926eae527a42cd797e3bb3e0b68818fa0a
danylo8 <ozai.avatar@gmail.com>
2025-06-06 21:54:46 -0400

    was able to create UI for BS. will probably need improvements later. This took so incredibly long. I stayed up till 2 am and still was only able to fix and make it work only now

--- BS.java
+++ BS.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -5,7 +9,7 @@ import java.util.Scanner;
 public class BS
 {
     static Scanner scan = new Scanner(System.in);
-    static String[] cardNum = {"A", "1", "2", "3", "4", "5", "6", "7", "J", "Q", "K"};
+    static String[] cardNum = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
     static String[] cardType = {"Hearts", "Spades", "Clubs", "Diamonds"};
     static ArrayList<String> originalDeck = new ArrayList<>();
     static ArrayList<String> playerDeck = new ArrayList<>();
@@ -13,16 +17,97 @@ public class BS
     static ArrayList<String> activeDeck = new ArrayList<>();
     static int expectedCard = 0;
     static int lastExpectedCard=0;
+    static int widthOfCards=0;
+    static int spacingOfCards=0;
     static String lastCard="";
     static String trueCard="";
-    static boolean playerTurn = true;
+    static boolean playerTurn = false;
     static boolean oppsTurn = false;
     static boolean PlayerCallBS = false;
     static boolean OppCallBS = false;
+    static boolean checkBS=false;
+
+
+    static JTextArea log=new JTextArea();
+    static JButton button= new JButton("Call BS");
+    static JLabel activeDeckUI=new JLabel();
+    static JPanel playerDeckUI=new JPanel();
+    static JLabel mascotImageUI=new JLabel();
+    static JLabel playerImageUI=new JLabel();
 
     public BS()
     {
-        System.out.println("Welcome to BS! If you need to learn the rules, you may read them at the bottom of the page\n");
+        JFrame frame = new JFrame();
+        frame.setTitle("BS");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(720,600);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setResizable(false);
+
+
+        button=new JButton("Call BS");
+        button.setBounds(300,400,100,20);
+        frame.add(button);
+
+        button.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 checkBS=true;
+                 if (expectedCard >= 2
+                 ) {
+                     log.append("Player called BS\n");
+                 }
+                 playerMove();
+
+                 checkBS=false;
+         }});
+
+
+        log.setEditable(false);
+        frame.setVisible(true);
+
+
+        JScrollPane scrollPane=new JScrollPane(log);
+        frame.add(scrollPane);
+        frame.setVisible(true);
+        scrollPane.setBounds(0,430,490,100);
+        frame.add(scrollPane);
+
+        frame.setVisible(true);
+
+
+        ImageIcon mascotImage=new ImageIcon("images/Mascot_Cards.png");
+        mascotImageUI= new JLabel(mascotImage);
+        mascotImageUI.setBounds(270,-30,160,200);
+        frame.add(mascotImageUI);
+        mascotImageUI.revalidate();
+        mascotImageUI.repaint();
+
+        ImageIcon playerImage=new ImageIcon("images/Player_Cards.png");
+        playerImageUI= new JLabel(playerImage);
+        playerImageUI.setBounds(495,350,160,200);
+        frame.add(playerImageUI);
+        playerImageUI.revalidate();
+        playerImageUI.repaint();
+
+        ImageIcon backCard=new ImageIcon("cards/BACK.png");
+
+        activeDeckUI= new JLabel(backCard);
+        activeDeckUI.setBounds(320,150,60,90);
+        frame.add(activeDeckUI);
+        frame.setVisible(true);
+        activeDeckUI.revalidate();;
+        activeDeckUI.repaint();
+
+
+        playerDeckUI= new JPanel(null);
+        playerDeckUI.setBounds(20,280,680,100);
+        frame.add(playerDeckUI);
+
+        log.append("Welcome to BS!\n");
+        System.out.println("Welcome to BS!");
+        System.out.println("");
         intitalizeDeck(originalDeck);
         shuffleDeck(originalDeck);
 
@@ -36,33 +121,30 @@ public class BS
                 oppsDeck.add(originalDeck.get(i));
                 }
         }
+        playerCardsUI();
+        whoShouldStart();
+        if (playerTurn==true) {
+            playerMove();
+        }
+        if (oppsTurn==true) {
+            oppMove();
+        }
+
 
 
         
-        while(playerDeck.size()!=0 && oppsDeck.size()!=0)
-        {
-            if (activeDeck.size()==0)
-            {
-                whoHasAce();
 
-            }
-            if (playerTurn==true)
-            {
-                playerMove();
-            }
-
-            if(oppsTurn==true)
-            {
-                oppMove();
-            }
-        }
 
         if (playerDeck.size()==0) {
             System.out.println("Opp wins, player loses");
+            log.append("Opp wins, player loses");
+            log.append("");
         }
 
         if (oppsDeck.size()==0) {
             System.out.println("Player wins, opp loses");
+            log.append("Player wins, opp loses");
+            log.append("");
         }
 
     }
@@ -83,7 +165,44 @@ public class BS
             Collections.shuffle(shuffledDeck);
         }
 
-        public static void whoHasAce()
+        public static void playerCardsUI(){
+            playerDeckUI.removeAll();
+
+            widthOfCards=(playerDeck.size()-1)*20+60; //take all cards except first, multiplies by spacing and width of individual card
+            spacingOfCards=(640-widthOfCards)/2; //640 is width of playerHandUI
+
+            for (int i=0; i<playerDeck.size(); i++)
+            {
+
+                ImageIcon card=new ImageIcon("cards/" + playerDeck.get(i) + ".png");
+
+                JButton cardButton=new JButton(card);
+                cardButton.setBounds(spacingOfCards+i*20, 0,60,90);
+
+                int indexOfCardPressed = i;
+                cardButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (indexOfCardPressed<playerDeck.size()) {
+                            String playedCard = playerDeck.remove(indexOfCardPressed);
+                            activeDeck.add(playedCard);
+                            log.append("You put down " + playedCard + "\n");
+
+                            playerTurn = false;
+                            oppsTurn = true;
+                            playerCardsUI();
+                            oppMove();
+                        }
+                    }
+                });
+
+                playerDeckUI.add(cardButton);
+            }
+            playerDeckUI.revalidate();;
+            playerDeckUI.repaint();
+        }
+
+        public static void whoShouldStart()
         {
 
             boolean playerHasAce=false;
@@ -107,7 +226,8 @@ public class BS
 
             if (playerHasAce==true)
             {
-                System.out.println("Player has Ace of Spades and will start");
+                log.append("Player has Ace of Spades and will start.\n");
+            //    System.out.println("Player has Ace of Spades and will start.\n");
                 playerTurn=true;
                 oppsTurn=false;
                 OppCallBS=false;
@@ -116,7 +236,8 @@ public class BS
 
             if(oppHasAce==true)
             {
-                System.out.println("Opp has Ace of Spades and will start");
+                log.append("Opp has Ace of Spades and will start. \n");
+              //  System.out.println("Opp has Ace of Spades and will start.");
                 playerTurn=false;
                 oppsTurn=true;
                 PlayerCallBS=false;
@@ -132,8 +253,9 @@ public class BS
 
             System.out.println("Your turn. Your cards: " + playerDeck);
             System.out.println("Do you want to place down a card or call BS?");
-            String choice = scan.nextLine().trim();
-            if (choice.equals("card"))
+         //   String choice = scan.nextLine().trim();
+            String choice = "";
+            if (choice.equals("if UI breaks change this :<("))
             {   for (int i=1; i<playerDeck.size(); i++)
                 {
                 System.out.println(i + ": " + playerDeck.get(i));
@@ -143,16 +265,16 @@ public class BS
                 String playedCard = playerDeck.remove(Integer.parseInt(choice1));
                 activeDeck.add(playedCard);
                 System.out.println("You put down: " + playedCard);
-                expectedCard = expectedCard + 1;
+                expectedCard = (expectedCard + 1)%cardNum.length;
                 playerTurn = false;
                 PlayerCallBS=false;
                 oppsTurn = true;
             }
 
-            if (choice.equals("BS") && PlayerCallBS==true)
+            if ((choice.equals("BS") && PlayerCallBS==true) || checkBS==true && expectedCard>=2)
             {
                 lastCard = activeDeck.get(activeDeck.size() - 1);
-                if (expectedCard != 0) {
+                if (expectedCard == 0) {
                     lastExpectedCard = cardNum.length - 1;
                 } else {
                     lastExpectedCard = expectedCard - 1;
@@ -160,18 +282,26 @@ public class BS
 
                 trueCard = lastCard.substring(0, lastCard.indexOf(" "));
 
-                if (!(trueCard.equals(lastExpectedCard))) {
+                if (!trueCard.equals(cardNum[lastExpectedCard])) {
                     System.out.println("Opp was BSing their card. Opp picks up the deck");
                     System.out.println("ExpectedCard: " + lastExpectedCard);
                     System.out.println("True card:" + trueCard);
+
+                    log.append("Opp was BSing their card. Opp picks up the deck\n");
+                    log.append("ExpectedCard: " + lastExpectedCard + "\n");
+                    log.append("True card:" + trueCard + "\n");
+
                     lastExpectedCard=0;
                     playerTurn = false;
                     oppsTurn = true;
                     for (int i = 0; i < activeDeck.size(); i++) {
                         oppsDeck.add(activeDeck.get(i));
-                        activeDeck.clear();
+
                     }
+                    activeDeck.clear();
+                    playerCardsUI();
                     System.out.println("They now have " + oppsDeck);
+                    log.append("They now have " + oppsDeck + "\n");
                 } else {
                     System.out.println("Opp put down the right card. Player picks up the deck.");
                     lastExpectedCard=0;
@@ -179,22 +309,34 @@ public class BS
                     oppsTurn = true;
                     for (int i = 0; i < activeDeck.size(); i++) {
                         playerDeck.add(activeDeck.get(i));
-                        activeDeck.clear();
+
                     }
+                    activeDeck.clear();
                     PlayerCallBS=false;
+                    playerCardsUI();
                     System.out.println("You now have " + playerDeck);
+                    log.append("You now have " + playerDeck + "\n");
             }}}
 
             public static void oppMove()
             {
+                if (activeDeck.size()<=1)
+                {
+                    OppCallBS=false;
+                    whoShouldStart();
+
+                }
+                expectedCard= (expectedCard+1)%cardNum.length;
                 OppCallBS=true;
                 PlayerCallBS=false;
-                if (Math.random()<0.3 && OppCallBS==true) {
+                if (Math.random()<0.3 && OppCallBS==true && oppsTurn==true && expectedCard>=2) {
                     OppCallBS=false;
                     System.out.println("Opp calls BS");
+                    log.append("Opp calls BS\n");
                     lastCard = activeDeck.get(activeDeck.size() - 1);
 
-                    if (expectedCard != 0) {
+
+                    if (expectedCard == 0) {
                         lastExpectedCard = cardNum.length - 1;
                     } else {
                         lastExpectedCard = expectedCard - 1;
@@ -202,36 +344,52 @@ public class BS
 
                     trueCard = lastCard.substring(0, lastCard.indexOf(" "));
 
-                    if (!(trueCard.equals(lastExpectedCard))) {
+                    if (!trueCard.equals(cardNum[lastExpectedCard])) {
                         System.out.println("Player was BSing their card. Player picks up the deck");
                         System.out.println("ExpectedCard: " + lastExpectedCard);
                         System.out.println("True card:" + trueCard);
+
+                        System.out.println("Player was BSing their card. Player picks up the deck\n");
+                        System.out.println("ExpectedCard: " + lastExpectedCard + "\n");
+
+                        System.out.println("True card:" + trueCard + "\n");
+
                         lastExpectedCard = 0;
                         playerTurn = true;
                         oppsTurn = false;
                         for (int i = 0; i < activeDeck.size(); i++) {
                             playerDeck.add(activeDeck.get(i));
-                            activeDeck.clear();
                         }
+                        activeDeck.clear();
+                        playerCardsUI();
                         System.out.println("You now have " + playerDeck);
+                        log.append("You now have " + playerDeck + "\n");
                     } else {
                         System.out.println("Player put the right card. Opp picks up the deck.");
+                        log.append("Player put the right card. Opp picks up the deck.\n");
                         lastExpectedCard = 0;
                         for (int i = 0; i < activeDeck.size(); i++) {
                             oppsDeck.add(activeDeck.get(i));
-                            activeDeck.clear();
+
                         }
+                        activeDeck.clear();
                         playerTurn = true;
                         oppsTurn = false;
-                        System.out.println("They now have " + oppsDeck);
-                    }}
+                        playerCardsUI();
+                        log.append("They now have " + oppsDeck+"\n");
+                        System.out.println("They now have " + oppsDeck+"\n");
+                    }
 
+                }
+
+                        log.append("Opp is placing a card. The expected card is: " + cardNum[expectedCard] + "\n");
                         System.out.println("Opp is placing a card. The expected card is: " + cardNum[expectedCard]);
-                        String playedCard = playerDeck.remove(0);
+                        String playedCard = oppsDeck.remove(0);
                         activeDeck.add(playedCard);
                         playerTurn = true;
                         oppsTurn = false;
                         OppCallBS=true;
+
 
 
                 }
--- Main.java
+++ Main.java
@@ -1,60 +1,37 @@
 import java.util.Scanner;
+import javax.swing.JFrame;
 
 public class Main
 {
-    static String[] cardNum = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    static String[] cardType = {"Hearts", "Spades", "Clubs", "Diamonds"};
-    static ArrayList<String> mainDeck = new ArrayList<>();
-    static ArrayList<String> activeDeck = new ArrayList<>();
+
     static Scanner scan = new Scanner(System.in);
-    int playerCounts = 0;
-    int oppsCounts = 0;
+
     public static void main(String[] args)
     {
+
         System.out.println("What card game would you like to play?");
-        System.out.println("There is Cuarenta, Spit, Spoons, and BullSh!t");
+        System.out.println("There is Spit, Spoons, and BS");
         System.out.println("Type in the exact title of the game you would like to play");
         String choice = scan.nextLine().trim();
-        if(choice.equals("Cuarenta"))
-        {
-            //System.out.println("Sorry, i don't know how to speak Spanish, or Portuguese");
-            Cuarenta gameCuarenta = new Cuarenta();
-        }else if(choice.equals("Spit"))
+
+
+        if(choice.equals("Spit"))
         {
             Spit spitGame = new Spit();
+
         }else if(choice.equals("Spoons"))
         {
             //System.out.println("Sorry, but someone took the last of the disposible spoons to some party last night");
             Spoons spoonGame = new Spoons();
-        }else if(choice.equals("BullSh!t"))
+        }else if(choice.equals("BS"))
         {
             //System.out.println("Hey! No curse words!");
-            Bullsht bsGame = new Bullsht();
+
+            BS bsGame = new BS();
         }
+
     }
 
-    public static void intitalizeDeck(ArrayList<String> deck)
-    {
-        deck.clear();
-        for(int i = 0; i < cardType.length; i++)
-        {
-            for(int j = 0;j < cardNum.length; j++)
-            {
-                deck.add(cardNum[j] + " of " + cardType[i]);
-            }
-        }
+
     }
 
-    //Try to figure out a way to fix the code
-    public static void shuffleDeck(ArrayList<String> deck)
-    {
-        //String temp = "";
-        //for(int i = 0; i < deck.length; i++)
-        {
-            //temp = deck[i];
-            //deck[i] = deck[Math.random()*deck.length];
-            //deck[Math.random()*deck.length] = temp;
-            Collections.shuffle(deck);
-        }
-    }
-}
